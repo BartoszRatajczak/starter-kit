93187f4696bd7fdbc9d6d7a4a2831c92195e8967
Bartosz Ratajczak <ratajczak89@gmail.com>
2015-11-03 10:54:49 +0100

    BookDaoImpl.save moved to aspect

--- spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
@@ -4,18 +4,29 @@ package pl.spring.demo.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import pl.spring.demo.common.Sequence;
+import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.exception.BookNotNullIdException;
+import pl.spring.demo.to.BookTo;
 import pl.spring.demo.to.IdAware;
 
 @Aspect
 @Component
 public class BookDaoAdvisor {
+	
+	@Autowired
+    private Sequence sequence;
+	
+	@Autowired
+	private BookDao bookDao;
 
-    @Before(value = "@annotation(pl.spring.demo.annotation.NullableId)")
+	@Before(value = "@annotation(pl.spring.demo.annotation.NullableId)")
     public void before(JoinPoint joinPoint) throws Throwable {
     	checkNotNullId(joinPoint.getArgs()[0]);
+    	((BookTo) joinPoint.getArgs()[0]).setId(sequence.nextValue(bookDao.findAll()));
     }
 
     private void checkNotNullId(Object o) {
